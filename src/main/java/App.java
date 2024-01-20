060b963ef4ee61a5decf9d77db18674162b6cf1d
toniname <60040991+toniname@users.noreply.github.com>
2024-01-20 19:25:59 +0200

    code Review

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,38 +1,41 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+import static java.lang.System.*;
 
 public class App {
+    static byte winner = 0;
+    static char[] box = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    static byte input;
+    static byte rand;
+    static byte i = 0;
+    private static final AtomicBoolean boxAvailable = new AtomicBoolean(false);
+    private static final Scanner scan = new Scanner(in);
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        byte input;
-        byte rand;
-        byte i;
-        boolean boxAvailable = false;
-        byte winner = 0;
-        char box[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        System.out.println("Enter box number to select. Enjoy!\n");
+        out.println("Enter box number to select. Enjoy!\n");
+
 
         boolean boxEmpty = false;
         while (true) {
-            System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
-            if(!boxEmpty){
-                for(i = 0; i < 9; i++)
+            for (String s : Arrays.asList(STR."\n\n \{box[0]} | \{box[1]} | \{box[2]} ", "-----------", STR." \{box[3]} | \{box[4]} | \{box[5]} ", "-----------", STR." \{box[6]} | \{box[7]} | \{box[8]} \n")) {
+                out.println(s);
+            }
+            if (!boxEmpty) {
+                for (i = 0; i < 9; i++)
                     box[i] = ' ';
                 boxEmpty = true;
             }
 
-            if(winner == 1){
-                System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
+
+            if (winner == 1) {
+                out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
-            } else if(winner == 2){
-                System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
+            } else if (winner == 2) {
+                out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
-            } else if(winner == 3){
-                System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+            } else if (winner == 3) {
+                out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
             }
 
@@ -40,32 +43,31 @@ public class App {
                 input = scan.nextByte();
                 if (input > 0 && input < 10) {
                     if (box[input - 1] == 'X' || box[input - 1] == 'O')
-                        System.out.println("That one is already in use. Enter another.");
+                        out.println("That one is already in use. Enter another.");
                     else {
                         box[input - 1] = 'X';
                         break;
                     }
-                }
-                else
-                    System.out.println("Invalid input. Enter again.");
+                } else
+                    out.println("Invalid input. Enter again.");
             }
 
-            if((box[0]=='X' && box[1]=='X' && box[2]=='X') || (box[3]=='X' && box[4]=='X' && box[5]=='X') || (box[6]=='X' && box[7]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[3]=='X' && box[6]=='X') || (box[1]=='X' && box[4]=='X' && box[7]=='X') || (box[2]=='X' && box[5]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[4]=='X' && box[8]=='X') || (box[2]=='X' && box[4]=='X' && box[6]=='X')){
-                   winner = 1;
-                   continue;
+            if ((box[0] == 'X' && box[1] == 'X' && box[2] == 'X') || (box[3] == 'X' && box[4] == 'X' && box[5] == 'X') || (box[6] == 'X' && box[7] == 'X' && box[8] == 'X') ||
+                    (box[0] == 'X' && box[3] == 'X' && box[6] == 'X') || (box[1] == 'X' && box[4] == 'X' && box[7] == 'X') || (box[2] == 'X' && box[5] == 'X' && box[8] == 'X') ||
+                    (box[0] == 'X' && box[4] == 'X' && box[8] == 'X') || (box[2] == 'X' && box[4] == 'X' && box[6] == 'X')) {
+                winner = 1;
+                continue;
             }
 
-            boxAvailable = false;
-            for(i=0; i<9; i++){
-                if(box[i] != 'X' && box[i] != 'O'){
-                    boxAvailable = true;
+            boxAvailable.set(false);
+            for (i = 0; i < 9; i++) {
+                if (box[i] != 'X' && box[i] != 'O') {
+                    boxAvailable.set(true);
                     break;
                 }
             }
 
-            if(boxAvailable == false){
+            if (!boxAvailable.get()) {
                 winner = 3;
                 continue;
             }
@@ -78,13 +80,13 @@ public class App {
                 }
             }
 
-            if((box[0]=='O' && box[1]=='O' && box[2]=='O') || (box[3]=='O' && box[4]=='O' && box[5]=='O') || (box[6]=='O' && box[7]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[3]=='O' && box[6]=='O') || (box[1]=='O' && box[4]=='O' && box[7]=='O') || (box[2]=='O' && box[5]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[4]=='O' && box[8]=='O') || (box[2]=='O' && box[4]=='O' && box[6]=='O')){
+            if ((box[0] == 'O' && box[1] == 'O' && box[2] == 'O') || (box[3] == 'O' && box[4] == 'O' && box[5] == 'O') || (box[6] == 'O' && box[7] == 'O' && box[8] == 'O') ||
+                    (box[0] == 'O' && box[3] == 'O' && box[6] == 'O') || (box[1] == 'O' && box[4] == 'O' && box[7] == 'O') || (box[2] == 'O' && box[5] == 'O' && box[8] == 'O') ||
+                    (box[0] == 'O' && box[4] == 'O' && box[8] == 'O') || (box[2] == 'O' && box[4] == 'O' && box[6] == 'O')) {
                 winner = 2;
-                continue;
             }
         }
 
     }
+
 }
